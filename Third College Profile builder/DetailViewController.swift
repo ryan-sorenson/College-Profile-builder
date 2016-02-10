d097299bc27dfb3b0c7ae959084dd61fce7d654e
Student <student@bluc02mg0l9fh51.local>
2016-02-10 09:40:29 -0600

    Finished code for the detail view Controller

--- Third College Profile builder/DetailViewController.swift
+++ Third College Profile builder/DetailViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var collegeTextField: UITextField!
+    @IBOutlet weak var numberOfStudentsTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var college: College!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberOfStudentsTextField.text = String(college.numberOfStudents)
+        locationTextField.text = college.location
+        collegeTextField.text = college.name
+        imageView.image = college.image
 
+    }
+    @IBAction func onTappedSaveButton(sender: AnyObject) {
+        college.name = collegeTextField.text!
+        college.location = locationTextField.text!
+        college.numberOfStudents = Int(numberOfStudentsTextField.text!)!
+        
     }
 
 }

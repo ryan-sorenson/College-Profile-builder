6aaefecd538107d565b2949b06fa17675d29d151
Student <student@bluc02mg0l9fh51>
2016-05-25 20:37:52 -0500

    Created Map view controller and connected button from detail view controller to the map view controller

---
+++ Third College Profile builder/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Third College Profile builder
+//
+//  Created by Student on 5/25/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

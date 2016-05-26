cc4dada1e466b34343b429ea9a3390642fa9d783
Student <student@bluc02mg0l9fh51>
2016-05-25 21:18:29 -0500

    mostly done with map view controller

--- Third College Profile builder/MapViewController.swift
+++ Third College Profile builder/MapViewController.swift
@@ -22,5 +22,47 @@ class MapViewController: UIViewController, UITextFieldDelegate {
         textView.delegate = self
 
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let locationName = textField.text!
+        textField.resignFirstResponder()
+        findLocation(locationName)
+        return true
+    }
+    func findLocation(locationName: String){
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(locationName) { (placemarks, error) -> Void in
+            if error != nil{
+                print(error)
+                
+            }
+            else{
+                if placemarks!.count > 1{
+                    let alert = UIAlertController(title: "Select Location", message: nil, preferredStyle: .ActionSheet)
+                    for placemark in placemarks!
+                    {
+                        let locationAction = UIAlertAction(title: placemark.name!, style: .Default, handler: { (action) -> Void in
+                            self.displayMap(placemark)
+                        })
+                    }
+                }
+                else if placemarks?.count == 1 {
+                    let placemark = placemarks!.first! as CLPlacemark
+                    self.displayMap(placemark)
+                }
+            }
+        }
+    }
+    
+    func displayMap(placemark: CLPlacemark){
+        textView.text = placemark.name
+        let center = placemark.location!.coordinate
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        let pin = MKPointAnnotation
+        pin.coordinate = center
+        pin.title = placemark.name
+        mapView.addAnnotation(pin)
+        mapView.setRegion(region, animated: true)
+    }
 }
